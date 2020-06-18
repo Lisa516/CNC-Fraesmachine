2be28eced29aff7bc5a565c715a3b9a5f96784ae
JoshiK8 <66957161+JoshiK8@users.noreply.github.com>
2020-06-18 12:54:07 +0200

    First Try

--- Fraeskopf.java
+++ Fraeskopf.java
@@ -1,1 +1,19 @@
+public class Fraeskopf
+{
+	private double _geschwindigkeit;
+	
+	public double Geschwindigkeit
+	{
+		get {return _geschwindigkeit;}
+		set {_geschwindigkeit = value;}
+	}
+	
+	private boolean _bohrenAktiv;
+	
+	public boolean BohrenAktiv
+	{
+		get {return _bohrenAktiv;}
+		set {_bohrenAktiv = value;}
+	}
+}
 
