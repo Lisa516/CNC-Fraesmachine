7704951cecee419c8a9ae76888d5261d94434875
I516691 <lisa.pruem@sap.com>
2020-06-18 14:25:14 +0200

    Second Try Adding A Package

---
+++ grafisch/src/grafisch/cfsi.java
@@ -0,0 +1,10 @@
+package grafisch;
+
+public class cfsi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ grafisch/src/grafisch/dmö.java
@@ -0,0 +1,5 @@
+package grafisch;
+
+public class dmö {
+
+}
