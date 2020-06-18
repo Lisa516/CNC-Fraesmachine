7905837bcb2b4da3fa3b0ccabd2917099264d952
JoshiK8 <joshuakraemer@gmx.de>
2020-06-18 14:28:36 +0200

    toString für Fräse

--- Fraeskopf.java
+++ Fraeskopf.java
@@ -1,1 +1,38 @@
-
+public class Fraeskopf
+{
+	private double _geschwindigkeit;
+	
+	public double Geschwindigkeit
+	{
+		get {return _geschwindigkeit;}
+		set {_geschwindigkeit = value;}
+	}
+	
+	private boolean _fraesenAktiv;
+	
+	public boolean fraesenAktiv
+	{
+		get {return _bohrenAktiv;}
+		set {_bohrenAktiv = value;}
+	}
+	
+	public String toString()
+	{
+		String statusAusgabe;
+		
+		if (fraesestatus == 0)
+		{
+			statusAusgabe = "aus";
+		}
+		
+		else
+		{
+			statusAusgabe = "ein";
+		}
+	}
+	
+	public Fraeskopf ()
+	{
+		
+	}
+}
