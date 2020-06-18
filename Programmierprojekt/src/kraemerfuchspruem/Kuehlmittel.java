62f882d9fd7e689114e59a0ef404dde0ec8993d4
I516691 <lisa.pruem@sap.com>
2020-06-18 16:33:32 +0200

    Redoing class Kuehlmittel

--- Programmierprojekt/src/kraemerfuchspruem/Kuehlmittel.java
+++ Programmierprojekt/src/kraemerfuchspruem/Kuehlmittel.java
@@ -1,1 +1,19 @@
-
+public class Kuehlmittel {
+	
+private boolean kuehlmittelstatus = false;
+	// 0-> Kühlmittel aus; 1-> Kühlmittel an
+	
+	public String toString()
+	{
+		String statusAusgabe;
+		if (kuehlmittelstatus == 0)
+		{
+		statusAusgabe = "aus";
+		}
+		else
+		{
+		statusAusgabe = "ein";	
+		}
+	return "Hallo";	
+	}		
+}
