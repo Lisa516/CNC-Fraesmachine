5397a7a675cda2c39efb8134fc0c767a97b0ac05
I516691 <lisa.pruem@sap.com>
2020-06-18 13:13:17 +0200

    First Operations and Attributes for Spindel

--- Spindel.java
+++ Spindel.java
@@ -1,1 +1,26 @@
+public class Spindel {
+	public boolean status;
+	// 0 = aus, 1 = ein
+	public boolean drehrichtung;
+	// 0 = im Uhrzeigersinn (Rechtslauf), 1 = gegen den Uhrzeigersinn (Linkslauf)
+	
+	
+	public void stopp() {
+		if (status == 1) {
+			status == 0;
+		}
+		else {
+			//TODO: Fehlermeldung, dass Spindel nicht läuft
+		}
+	}
+	
+	public void start() {
+		if (status == 0) {
+			status == 0;
+		}
+		else {
+			//TODO: Fehlermeldung, dass Spindel läuft 
+		}
+	}
 
+}
