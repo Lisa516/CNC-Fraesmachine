3b82b564eba5b372a996d2d0e3fceaf7a2589f9a
I516691 <lisa.pruem@sap.com>
2020-07-07 01:03:40 +0200

    Adding Error Handling Messages for double Stop/Go

--- view/src/application/ErrorHandling.java
+++ view/src/application/ErrorHandling.java
@@ -1,1 +1,30 @@
+package application;
 
+public class ErrorHandling extends Thread{
+	public ErrorHandling() {
+	}
+	
+	public static String fraeskopfRunning() {
+		//TODO: Rephrase following
+		return "Der Fraeskopf wurde versucht anzuschalten, obwohl er bereits läuft.";
+	}
+	
+	public static String fraeskopfStopped() {
+		//TODO: Rephrase following
+		return "Der Fraeskopf wurde versucht auszuschalten, obwohl er nicht lief.";
+	}
+	
+	public static String spindelStopped() {
+		//TODO: Rephrase following
+		return "Die Spindel wurde versucht auszuschalten, obwohl sie nicht lief.";
+	}
+	
+	public static String spindelRunning() {
+		//TODO: Rephrase following
+		return "Die Spindel wurde versucht anzuschalten, obwohl sie läuft.";
+	}
+	
+	public void run() {
+		
+	}
+}
