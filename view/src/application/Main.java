07afc726dc41b2e2e9e26f69285ef547ea2d55e8
I516691 <lisa.pruem@sap.com>
2020-07-08 09:37:48 +0200

    JavaFX improvements

--- view/src/application/Main.java
+++ view/src/application/Main.java
@@ -1,15 +1,47 @@
 package application;
 
 import javafx.application.Application;
-import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
+	
+	public static Fraeskopf fraeskopf = new Fraeskopf();
+	public static Kuehlmittel kuehlmittel = new Kuehlmittel();
+	public static ErrorHandling errorHandling = new ErrorHandling();
+	
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			BorderPane root = new BorderPane();
+			Scene scene = new Scene(root,1000,560);
+			
+			Label position = new Label("Position: " + fraeskopf._getPosition());
+			Label kuehlmittelStatus = new Label("Kuehlmittelstatus: " + kuehlmittel.toString());
+			
+			root.setTop(position);
+			root.setTop(kuehlmittelStatus);
+			
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			
+			primaryStage.setScene(scene);
+			primaryStage.setResizable(false);
+			//kein Resizen möglich machen, um Fehler zu verhindern
+			primaryStage.centerOnScreen();
+			primaryStage.setTitle("Fraesmaschinenapplikation");
+			primaryStage.show();
+			
+			
+		} catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+		
 	public static void main(String[] args) {
-		//launch(args);
+		launch(args);
 		final String HOME;
 		Thread t1 = new Kuehlmittel();
 		Thread t2 = new Fraeskopf();
@@ -19,24 +51,6 @@ public class Main extends Application {
 		
 	}
 	
-public static Fraeskopf fraeskopf = new Fraeskopf();
-public static Kuehlmittel kuehlmittel = new Kuehlmittel();
-public static ErrorHandling errorHandling = new ErrorHandling();
-	
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,1000,560);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-			Label position = new Label(fraeskopf._getPosition());
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	
-	}
 	
 	public static void run() {
 	}
